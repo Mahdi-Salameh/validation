886ec3353a2f7ddd42a3b6bdd48972c87b7ce309
WaelAlBasha <72439498+WaelAlBasha@users.noreply.github.com>
2021-12-01 17:35:36 +0100

    Add ParentStore_Proxy

--- kernel.py
+++ kernel.py
@@ -1,16 +1,31 @@
 class Transition_Relation:
-    def initial(self) : pass
-    def next(self,c) : pass
+    def initial(self): pass
+    def next(self, c): pass
 
 
 class AcceptingSet:
-    def is_accepting(c) : pass
-    
+    def is_accepting(c): pass
 
-class identity_proxy:
-    def __init__(self,operand):
+
+class Identity_Proxy:
+    def __init__(self, operand):
         self.operand = operand
-    
-    def __getattr__(self,attr):
+
+    def __getattr__(self, attr):
         return getattr(self.operand, attr)
-        
+
+
+class ParentStore_Proxy (Identity_Proxy):
+
+    def __init__(self, op):
+        super().__init__(op)
+        self.parent = {}
+
+    def next(self, conf):
+        nS = self.operand.next(conf)
+
+        for n in nS:
+            if n not in self.parent:
+                self.parent[n] = conf
+
+        return nS
