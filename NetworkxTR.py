dcc609bb3ffce7bf5ca8cf4f48c5e8e008e98721
Najla-OJEIMI <ojeimina@gmail.com>
2021-12-01 14:18:03 +0100

    Adding Class Networkx file

---
+++ NetworkxTR.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created Tuesday November 30 21:36:20 2021
+
+@author: ALBasha_OJEIMI_SALAMEH
+"""
+
+class NetworkxTR:
+    
+    def _init_(self, nom, arcs):
+        self.__nom = nom
+        self.__arcs = arcs
+
+
+    pass
