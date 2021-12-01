b7460b9472a48e2c685918fbd8b1801002b47dcd
WaelAlBasha <72439498+WaelAlBasha@users.noreply.github.com>
2021-12-01 17:13:24 +0100

    Modif Is_Safe

--- algorithms.py
+++ algorithms.py
@@ -5,24 +5,24 @@ Created Tuesday November 30 21:36:20 2021
 @author: ALBasha_OJEIMI_SALAMEH
 """
 
-def is_safe_bfs(graph):
-        known = [] #Known
-        frontier = [] #Frontier #list_des_noeuds
-        at_start = True #init
 
-        while frontier or at_start:
-            if at_start:
-                neighbours = graph.initial()
-                at_start = False
-            else:
-                neighbours = graph.next( frontier.pop(0) )
-            for n in neighbours:
-                if graph.is_accepting(n):
-                    return False
+def find_accepting_bfs(graph):
+    known = []  # Known
+    frontier = []  # Frontier #list_des_noeuds
+    at_start = True  # init
 
-                if n not in known:
-                    known.append(n)
-                    frontier.append(n)
-            
-        print(known)
+    while frontier or at_start:
+        if at_start:
+            neighbours = graph.initial()
+            at_start = False
+        else:
+            neighbours = graph.next(frontier.pop(0))
+        for n in neighbours:
+            if graph.is_accepting(n):
+                return True, n
 
+            if n not in known:
+                known.append(n)
+                frontier.append(n)
+
+    return False, None
