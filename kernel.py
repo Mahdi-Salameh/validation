8691abcb4818ea884a097c32b9e5f267409f3d3f
Najla-OJEIMI <ojeimina@gmail.com>
2021-12-01 17:14:22 +0100

    Adding classes

--- kernel.py
+++ kernel.py
@@ -6,3 +6,11 @@ class Transition_Relation:
 class AcceptingSet:
     def is_accepting(c) : pass
     
+
+class identity_proxy:
+    def __init__(self,operand):
+        self.operand = operand
+    
+    def __getattr__(self,attr):
+        return getattr(self.operand, attr)
+        
