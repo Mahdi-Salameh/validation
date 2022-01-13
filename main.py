73e669d2cc0426392b37a2d738cdeb297dbb3efe
Mahdi-Salameh <Mahdi_salameh@hotmail.com>
2022-01-13 23:49:31 +0100

    modif main

--- main.py
+++ main.py
@@ -16,12 +16,14 @@ from networkx.classes.function import nodes
 from networkx.generators.classic import null_graph
 from algorithms import *
 from simpleGraph import *
-from BehaviorSoup import *
 from counter import *
 from souplang import *
 from Predicate import *
+from aliceBobV0 import *
+from hanoi import *
+from counter import *
 
-if __name__ == "__main__":
+def main_networkx():
 
     G = nx.DiGraph()
 
@@ -105,15 +107,127 @@ if __name__ == "__main__":
                 print("The node", L[i], "has the following neighbors :")
                 print([n for n in G.neighbors(L[i])])
 
-    # iterate()
+    iterate()
     g = NFA(G, ['A'], ['B', 'T'])
-    temp, n = find_accepting_bfs(g)
-    if temp:
-        print(n)
+    # temp, n = find_accepting_bfs(g)
+    # if temp:
+    #     print(n)
+    known = find_accepting_bfs(g)
+    print("Accessible nodes from A: ", known)
     plt.show()
 
-    Behavior_Object =
+    #semantics = BehSoupSemantics(counter(3))
+    # predicate_model_checker(semantics, lambda c: c.pc == 2)
+    # predicate_model_checker(semantics, lambda c: c.pc > 50)
 
-    semantics = BehSoupSemantics(counter(3))
-    predicate_model_checker(semantics, lambda c: c.pc == 2)
-    predicate_model_checker(semantics, lambda c: c.pc > 50)
+def main_hanoi_1():
+    # Input: number of disks
+    num_of_disks = 4
+
+    # Create three stacks of size 'num_of_disks'
+    # to hold the disks
+    src = createStack(num_of_disks)
+    dest = createStack(num_of_disks)
+    aux = createStack(num_of_disks)
+
+    tohIterative(num_of_disks, src, aux, dest)
+
+def main_hanoi_2():
+    print("-------------------------")
+    print("Guarde / Action")
+    print("Example 1: ")
+    hanoi_tower = ParentStore_Proxy(Hanoi(3,3))
+    for i, j in [(0, 1), (0, 2), (2, 1)]:
+        init = hanoi_tower.initial()[0]
+        guarde = guarde_def(i, j)
+        action = action_def(i, j)
+        g = guarde(init)
+        if g:
+            a = action(init)
+        print(f'{i},{j} : {"Vrai" if g else "Faux"} -> {init}')
+
+    print("Example 2: ")
+    init = hanoi_tower.initial()[0]
+    for i, j in [(0, 2), (0, 1), (2, 1), (0, 2), (1, 0), (1, 2), (0, 2)]:
+        guard = guarde_def(i, j)
+        action = action_def(i, j)
+        g = guard(init)
+        if g:
+            a = action(init)
+        print(f'{i},{j} : {"Vrai" if g else "Faux"} -> {init}')
+
+    print("-------------------------")
+    print("Soup")
+    soup = hanoi_soap(3, 3)
+    Behavior_Soup = BehSoupSemantics(soup)
+    init = Behavior_Soup.initial()[0]
+    print("First State: ", init)
+    actions = Behavior_Soup.actions(init)
+
+    if actions:
+        print("Possible action : ", inspect.getsource(action))
+        for action in actions:
+            execute = Behavior_Soup.execute(init, action)
+            print("Execution output : ", execute)
+
+    print("-------------------------")
+
+    print("STR2TR")
+    str = STR2TR(Behavior_Soup)
+    init = str.initial()[0]
+    next = str.next(init)
+    print("States after ", init, "are", next)
+
+    print("-------------------------")
+
+
+def main_counter():
+    semantics = BehSoupSemantics(counter(5))
+    print(semantics.initial())
+    print(semantics.actions(semantics.initial()[0]))
+    r = bfs(STR2TR(semantics))
+    print(r)
+    predicate_model_checker(semantics, lambda c: c.jpc == 2)
+    # print(r)
+    predicate_model_checker(semantics, lambda c: c.jpc > 50)
+    # print(r)
+
+def main_alice_bob():
+    semantics = BehSoupSemantics(aliceBob())
+    # print(semantics.initial())
+    # print(semantics.actions(semantics.initial()[0]))
+
+    # for action in semantics.actions(semantics.initial()[0]):
+    #     print(inspect.getsource(action))
+
+    # print(inspect.getsource(semantics.actions(semantics.initial()[0]) ))
+
+    r = bfs(STR2TR(semantics))
+    print("States: ", r) 
+
+    predicate_model_checker(semantics, lambda c: c.bpc == 0)
+
+    print("Deadlock Test: ", end=" ")
+    predicate_model_checker(semantics, lambda c: len(semantics.actions(c)) == 0 )
+    print("Critical section Test: ", end=" ")
+    predicate_model_checker(semantics, lambda c: c.apc == 1 and c.bpc == 1 )
+
+
+if __name__ == "__main__":
+    print("1: Graph")
+    print("2: Hanoi V1")
+    print("3: Hanoi V2")
+    print("4: Counter")
+    print("5: AliceBob")
+    option = input("What is your choice? ")
+
+    if option == '1':
+        main_networkx()
+    elif option == '2':
+        main_hanoi_1()
+    elif option == '3':
+        main_hanoi_2()
+    elif option == '4':
+        main_counter()
+    elif option == '5':
+        main_alice_bob()
