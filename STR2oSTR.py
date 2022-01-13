73b84d4a796fcc49be4252dd1aacac1c23858490
Mahdi-Salameh <Mahdi_salameh@hotmail.com>
2022-01-13 18:01:29 +0100

    add kripke, modif str2ostr

--- STR2oSTR.py
+++ STR2oSTR.py
@@ -7,35 +7,11 @@ class STR2oSTR:
     def initial(self):
         return self.operand.initial()
 
-    # def actions(self,c):
-    #     return self.operand.actions(c)
+    def actions(self, c):
+        return self.operand.actions(c)
 
-    # def execute(self,source,a):
-    #     target = self.operand.execute(c,a)
-    #     return (source,a,target),target
+    def execute(self, c, source, a):
+        target = self.operand.execute(c,a)
+        return (source, a, target), target
 
-    def actions(self,source):
-        synchronons_actions = []
-        kripke_src, buchi_src = source
-        if kripke_src in None:
-            for k_target in self.lhs.initial():
-                self.get_synchronons_actions(k_target,buchi_src,synchronons_actions)
-            return synchronons_actions
-        k_actions = self.lhs.actions(kripke_src)
-        num_actions = len(k_actions)
-        for ka in k_actions:
-            _,k_target = self.lhs.execute(kripke_src,ka)
-            if k_target is None:
-                num_action -= 1
-                self.get_synchronons_actions(k_target,buchi_src,synchronons_actions)
-            if num_actions == 0:
-                self.get_synchronons_actions(kripke_src,buchi_src,synchronons_actions)
-
-    def get_synchronons_actions(self,kc,bc,is_synca):
-        buchi_actions = self.rhs.actions(kc,bc)
-        return is_synca.extend(map(lambda ba:(kc,ba),buchi_actions))
-
-    def execute(self,action,conf):
-        ktarget,baction = action
-        _,bsrc = conf
-        return ktarget,self.rhs.execute(ktarget,baction,bsrc) 
+    
