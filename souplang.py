3dd4a9b3fd27c1763f87ca262a908fac661a9a95
Najla-OJEIMI <ojeimina@gmail.com>
2022-01-13 17:30:40 +0100

    Organisation

--- souplang.py
+++ souplang.py
@@ -2,6 +2,22 @@ from kernel import*
 import copy
 
 
+class Behavior_Soup:
+    def __init__(self, conf):
+        self.initial = conf
+        self.behaviors = []
+
+    def add(self, n, g, a):
+        self.behaviors.append(Behavior(n, g, a))
+
+
+class Behavior:
+    def __init__(self, name, g, a):
+        self.name = name
+        self.action = a
+        self.guard = g
+
+
 class BehSoupSemantics(SemanticTransitionRelation):
     def __init__(self, program):
         self.soup = program
